fe2c4655865c4d73de13f29cca5b6387eb3c5ae0
Shashank Srivastava <shashank21005@gmail.com>
2025-06-28 08:44:09 +0000

    feat: Implements _run_target() method
    THe specific target running logic is implemented and linters are used to
    format the code.
    
    Signed-off-by: Shashank Srivastava <shashank21005@gmail.com>

--- src/test_runner.py
+++ src/test_runner.py
@@ -3,23 +3,30 @@ This module provides TestRunner class to manage the test execution.
 """
 
 import logging
-import subprocess
 import os
+import subprocess
+import time
+from subprocess import PIPE, Popen, run
+
+import yaml
+
 from target_setup import TargetSetup
 
 logging.basicConfig(
     level=logging.INFO,  # Set the logging level
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
 )
+
 
 class TestRunner:
     """
     This TestRunner class is designed to manage the test execution.
     """
+
     def __init__(self, target: TargetSetup) -> None:
         """
         Initialize the TestRunner with a specific target configrations.
-        
+
         :param targets: Specific TargetSetup object for TestRunner.
         """
         self.target = target
@@ -27,39 +34,78 @@ class TestRunner:
     def _build_target(self) -> None:
         """
         Build the target setup.
-        
+
         This method will execute the build configuration for the target.
         """
-        
+
         build_script_path = os.path.join(self.target.build_config.dir, "build")
-        
+
         if not os.path.exists(build_script_path):
             raise FileNotFoundError(f"Build script directory does not exist: {build_script_path}")
-        
+
         print("Building target:", self.target.id)
 
         result = subprocess.run(
-            ["bash", build_script_path], 
-            cwd=self.target.build_config.dir, 
-            capture_output=True, 
-            text=True
-            )
+            ["bash", build_script_path],
+            cwd=self.target.build_config.dir,
+            capture_output=True,
+            text=True,
+        )
 
         self.build_stdout_file_path = self._write_log_file(
             self.target.build_config.dir, "build_stdout.log", result.stdout
-            )
+        )
         self.build_stderr_file_path = self._write_log_file(
             self.target.build_config.dir, "build_stderr.log", result.stderr
-            )
+        )
 
-    def _run_target(self, run_target_directory: str) -> None:
+    def _run_target(self, run_target_dir: str) -> None:
         """
         Run the target setup.
-        
+
         This method will execute the run configurations for the target.
         """
+        run_script_path = os.path.join(run_target_dir, "run")
+        run_log_path = os.path.join(run_target_dir, "run.log")
+        curl_log_path = os.path.join(run_target_dir, "run_curl.log")
 
-        pass
+        with open("config.yaml", "r") as f:
+            app_config = yaml.safe_load(f)
+
+        networking_enabled = app_config.get("networking", False)
+        curl_command = app_config.get("testing_command", "curl http://localhost:8080")
+
+        # Start run script in background
+        with open(run_log_path, "w") as run_log_file:
+            process = subprocess.Popen(
+                ["bash", run_script_path],
+                cwd=run_target_dir,
+                stdout=run_log_file,
+                stderr=run_log_file,
+            )
+
+        print("Sleep staerted for 8 seconds to allow unikernel to start")
+        time.sleep(5)
+
+        # If networking is enabled, send a curl request
+        if networking_enabled:
+            try:
+                print(f"Sending curl request: {curl_command} with timeout of 7 seconds")
+                curl_result = run(curl_command, capture_output=True, text=True, timeout=7)
+                with open(curl_log_path, "w") as f:
+                    f.write(curl_result.stdout)
+                    f.write(curl_result.stderr)
+            except Exception as e:
+                with open(curl_log_path, "w") as f:
+                    f.write(f"[✗] Curl request failed: {e}")
+                process.terminate()
+                return False
+
+        # Wait for the unikernel to finish or timeout
+        try:
+            process.wait(timeout=10)
+        except Exception as e:
+            process.terminate()
 
     def _is_build_successful(self, log_path: str) -> bool:
         """
@@ -74,7 +120,7 @@ class TestRunner:
         success_keywords = ["Build completed successfully"]
 
         try:
-            with open(log_path, 'r', encoding='utf-8') as f:
+            with open(log_path, "r", encoding="utf-8") as f:
                 for line in f:
                     if any(keyword.lower() in line.lower() for keyword in success_keywords):
                         return True
@@ -86,20 +132,21 @@ class TestRunner:
     def _test_target_build(self, build_target_directory: str) -> bool:
         """
         Test the target build.
-        
+
         This method will execute the test configurations for the target.
         """
 
-        if self._is_build_successful(self.build_stdout_file_path) or \
-            self._is_build_successful(self.build_stderr_file_path):
+        if self._is_build_successful(self.build_stdout_file_path) or self._is_build_successful(
+            self.build_stderr_file_path
+        ):
             return True
 
-        return True # TODO: Updated it to True for now, as we are not checking the build logs yet
+        return True  # TODO: Updated it to True for now, as we are not checking the build logs yet
 
     def _test_target_run(self, run_target_directory: str) -> None:
         """
         Test the target run.
-        
+
         This method will execute the test configurations for the target.
         """
         print(f"Testing target run in directory: {run_target_directory}")
@@ -120,38 +167,41 @@ class TestRunner:
         try:
             os.makedirs(directory, exist_ok=True)  # Ensure the directory exists
             file_path = os.path.join(directory, filename)
-            with open(file_path, 'w', encoding='utf-8') as f:
+            with open(file_path, "w", encoding="utf-8") as f:
                 f.write(data)
             print(f"[✓] Log written to {file_path}")
         except Exception as e:
             print(f"[✗] Failed to write log to {filename} in {directory}: {e}")
-        
-        return file_path if os.path.exists(file_path) else "Error writing log file"
 
+        return file_path if os.path.exists(file_path) else "Error writing log file"
 
     def run_test(self) -> None:
         """
         Run the test for the target setup.
-        
+
         This method will execute the build and run configurations for the target.
         """
         print(f"Running tests for target: {self.target.id}")
         # Build the target before running tests(upto 2 mins)
         self._build_target()
         # Test if the build was successful
-        build_success = self._test_target_build(self.target.build_config.dir) #TODO: Implement this method to check if the build was successful
-        
+        build_success = self._test_target_build(
+            self.target.build_config.dir
+        )  # TODO: Implement this method to check if the build was successful
+
         if build_success:
             print(f"[✓]Build successful for target: {self.target.id}")
-             # Iterate over each of the runs
+            # Iterate over each of the runs
             for idx, run_config in enumerate(self.target.run_configs):
-                
+
                 print(f"Running configuration: {run_config.dir}")
                 # print(f"Run configuration of {idx + 1} is {run_config.config}")
                 # Execute each run configuration (upto 5 secs)
                 self._run_target(run_config.dir)
                 # Test the running unikernel
-                self._test_target_run(self.target.build_config.dir) #TODO: This is required to be done symultaniously.
+                self._test_target_run(
+                    self.target.build_config.dir
+                )  # TODO: This is required to be done symultaniously.
         else:
             print(f"[✗] Build failed for target: {self.target.id}")
             return
