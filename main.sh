bd5d28b09a1637d03317d56ef1f3e0816903bcf9
Shashank Srivastava <shashank21005@gmail.com>
2025-07-04 17:51:15 +0000

    refactor: Update support for app dir from relative to absolute
    Update codebase to require an absolute path for the application directory instead of a relative path. This simplifies application path identification during testing.
    
    Signed-off-by: Shashank Srivastava <shashank21005@gmail.com>

--- main.sh
+++ main.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
+
+# Check if argument is missing
 if [ -z "$1" ]; then
-  echo "Usage: $0 <app_name_ver>"
+  echo "Usage: $0 <absolute_path_to_app>"
   exit 1
 fi
 
-app_name_ver="$1"
+app_path="$1"
+
+# Check if the path is absolute
+if [[ "$app_path" != /* ]]; then
+  echo "Error: Please provide an absolute path (it should start with '/')"
+  exit 1
+fi
 
 # Clean up any previous instances.
 ./scripts/utils/cleanup.sh
 
-# Setup new instance, passing the versioned path.
-./scripts/utils/setup.sh "/home/machine02/catalog/library/${app_name_ver}/"
+# Setup new instance, passing the absolute path.
+./scripts/utils/setup.sh "$app_path"
 
 # Run tests.
 ./scripts/utils/tests_runner.sh
